6edf5fc1d9e5248358a26a5f63bb51b62a8deb52
Nikhil758 <navalinikku@gmail.com>
2024-03-29 17:54:29 +0530

    HighOrder

---
+++ highord.js
@@ -0,0 +1,41 @@
+// const radius = [1, 2, 3];
+// const area = function(radius){
+//     return Math.PI * radius * radius;
+// }
+// const diameter = function(radius){
+//     return 2 * radius;
+// }
+// const calculate = function(radius, logic){ 
+//     const output = [];
+//     for(let i = 0; i < radius.length; i++){
+//         output.push(logic(radius[i]))
+//     }
+//     return output;
+// }
+// console.log(calculate(radius, area));
+// console.log(calculate(radius, diameter));
+
+
+
+// const users = [
+//     {firstName: 'John', lastName: 'Doe', age: 25},
+//     {firstName: 'Jane', lastName: 'Doe', age: 30},
+//     {firstName: 'Jack', lastName: 'Doe', age: 35},
+//     {firstName: 'Jill', lastName: 'Doe', age: 40},
+//     {firstName: 'Joe', lastName: 'Doe', age: 45},
+// ]
+
+// const result = users.map((user) => user.firstName + ' ' + user.lastName)
+// console.log(result);
+
+// const arr = [1, 2, 3, 4, 5];
+// const output = arr.filter((num) => num % 2) // filter out odd numbers
+// console.log(arr); // [1, 2, 3, 4, 5]
+// console.log(output);
+
+// let numbers = [5, 20, 100, 60, 1];
+// const maxValue = numbers.reduce((max, curr) => {
+//     if(curr > max) max = curr;
+//     return max;
+// });
+// console.log(maxValue);
