c7c37a44698bab48b86375f4e708a31ea00ef334
Nikhil758 <navalinikku@gmail.com>
2024-04-01 15:26:08 +0530

    modified hoisting

---
+++ hoisting.js
@@ -0,0 +1,26 @@
+// x = 5; 
+// console.log(x);
+// var x; 
+
+// var x = 5;
+// var y;  
+// console.log(x + " " + y);   
+// y = 7;
+
+// class Person {
+//     constructor(name, age) {
+//       this.name = name;
+//       this.age = age;
+//     }
+//   }
+//   let peter = new Person('Peter', 25); 
+//   console.log(peter);
+
+// let Person = class {
+//     constructor(name, age) {
+//       this.name = name;
+//       this.age = age;
+//     }
+//   }
+//   let peter = new Person('Peter', 25); 
+//   console.log(peter);
