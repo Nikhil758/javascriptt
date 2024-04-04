ace6aa995717c0c75bab17f2f781ec799f396210
Nikhil758 <navalinikku@gmail.com>
2024-04-04 17:55:24 +0530

    Trees and Graphs

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+function palindrome(data){
+    let length=data.length;
+    for (ch of data){
+        if (ch===data[length-1]){
+            length--;
+        }
+        else{
+            return false;
+        }
+        return true;
+    }
+}
+if (palindrome("leve")){
+    console.log("Palindrome");
+}
+else{
+    console.log("Not");
+}
