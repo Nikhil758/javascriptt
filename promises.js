7c5ad52f11084b3c7ae7d9e7321f863dc8b5d260
Nikhil758 <navalinikku@gmail.com>
2024-04-10 13:58:06 +0530

    [Nikhil]Added:Promises

--- promises.js
+++ promises.js
@@ -1,19 +1,21 @@
-let promise = new Promise(function (resolve, reject) {
-    const x = "gg";
-    const y = "gg"
-    if (x === y) {
-        resolve();
-    } else {
-        reject();
-    }
-});
-promise.
-    then(function () {
-        console.log('Success');
-    }).
-    catch(function () {
-        console.log('Some error has occurred');
-    });
+//Promises
+
+// let promise = new Promise(function (resolve, reject) {
+//     const x = "gg";
+//     const y = "gg"
+//     if (x === y) {
+//         resolve();
+//     } else {
+//         reject();
+//     }
+// });
+// promise.
+//     then(function () {
+//         console.log('Success');
+//     }).
+//     catch(function () {
+//         console.log('Some error has occurred');
+//     });
 
     
 
