31719c1cb2dbcc6a5648a1d51afdcf56796fd6d5
Nikhil758 <navalinikku@gmail.com>
2024-04-10 13:56:48 +0530

    [Nikhil]Added:Oops

--- oops.js
+++ oops.js
@@ -108,15 +108,15 @@
 
 // Method Overloading
 
-function add(a, b) {
-    if (typeof a === "number" && typeof b === "number") {
-      return a + b;
-    } else if (typeof a === "string" && typeof b === "string") {
-      return a + b;
-    } else {
-      return "Invalid arguments";
-    }
-  }
-  console.log(add(2,3));
-  console.log(add('2','3'));
-  console.log(add(true));
+// function add(a, b) {
+//     if (typeof a === "number" && typeof b === "number") {
+//       return a + b;
+//     } else if (typeof a === "string" && typeof b === "string") {
+//       return a + b;
+//     } else {
+//       return "Invalid arguments";
+//     }
+//   }
+//   console.log(add(2,3));
+//   console.log(add('2','3'));
+//   console.log(add(true));
